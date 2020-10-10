08093f9a913d914251dd63e4364466fc01934e9f
Yoan Belhous <belhous.yoan@gmail.com>
2020-10-10 13:15:53 +0200

    DONE: class created (sled, dwarf, gift) sled fill and gift creation works + sled filling animation | TODO: darf status & time | ISSUE: couldn't import index.js into index.html worked with <script> CODE </script>

--- src/index.js
+++ src/index.js
@@ -6,3 +6,73 @@ import './index.css'
 import $ from 'jquery'
 
 // TODO
+
+var sled;
+var dwarf;
+
+function onLoad() {
+    sled = new Sled();
+    dwarf = new Dwarf();
+}
+
+
+class Gift {
+    constructor(weight, time) {
+        this.weight = weight;
+        this.time = time;
+    }
+}
+
+class Sled {
+    constructor() {
+        this.limitWeight = 10;
+        this.currentWeight = 0;
+        this.gifts = [];
+    }
+}
+
+class Dwarf {
+    constructor() {
+        
+    }
+    static status() {
+        let working = false;
+        let waiting = false;
+        let available = false;
+    }
+    static working() {
+        let gift = Gift();
+        let sled = Sled();
+    }
+}
+
+function prepare(weight, time) {
+    if (sled.currentWeight + weight <= sled.limitWeight) {
+        sled.currentWeight += weight;
+        sled.gifts.push(new Gift(weight, time));
+        document.getElementById('sled-weight').innerHTML = sled.currentWeight;
+        console.log(sled.gifts);
+
+        let sledweightStatus = Math.trunc(((100 * sled.currentWeight) / sled.limitWeight) /25);
+        let sledClass = '';
+
+        switch(sledweightStatus) {
+            case 0:
+                sledClass = 'sled sled-1';
+                break;
+            case 1:
+                sledClass = 'sled sled-1';
+                break;
+            case 2:
+                sledClass = 'sled sled-2';
+                break;
+            case 3:
+                sledClass = 'sled sled-3';
+                break;
+            case 4:
+                sledClass = 'sled sled-4';
+                break;
+        }
+        document.getElementById('sled-status-img').className = sledClass;
+    }
+}
